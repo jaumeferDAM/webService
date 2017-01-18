49340412e154c2c32ce7dae7c2085a1aa199e368
lperpinya1974 <lperpinya1974@gmail.com>
2017-01-18 16:57:30 +0100

    Commit Initial

---
+++ src/java/bd/Conexion.java
@@ -0,0 +1,137 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Lluis_2
+ */
+public class Conexion {
+    Connection connection;
+    
+    public Conexion()
+    {
+        try {
+                Class.forName("oracle.jdbc.driver.OracleDriver");
+                connection = DriverManager.getConnection("jdbc:oracle:thin:@192.168.180.10:1521:INSLAFERRERI", "PROFEA1","1234");
+               // connection = DriverManager.getConnection("jdbc:oracle:thin:@ieslaferreria.xtec.cat:8081:INSLAFERRERI", "PROFEA1","1234");
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+}
+
+    public Connection getConnection() {
+        return connection;
+    }
+    
+    
+        public void finalizarConexion() throws SQLException
+        {
+            connection.close();
+        }
+  
+        public  boolean insertarCliente(Cliente cli) throws SQLException
+{
+       String sql = "INSERT INTO Cliente (Nombre, Telefono) VALUES (?, ?)";
+       PreparedStatement stmt = connection.prepareStatement(sql);
+       stmt.setString(1, cli.getNombre()); //stmt.setString(1, cli.getNombre);
+       stmt.setInt(2, cli.getTelefono());
+       int res = stmt.executeUpdate();
+       finalizarConexion();
+           
+ 
+      return (res == 1);
+}
+
+    
+ 
+public List<Cliente> obtenerClientes() throws SQLException
+        
+{
+      ResultSet rset;
+      List<Cliente> lista = new ArrayList();
+      String sql = "SELECT IdCliente, Nombre, Telefono FROM Cliente";
+      PreparedStatement stmt = getConnection().prepareStatement(sql);
+      rset = stmt.executeQuery();
+      while (rset.next())
+      {
+          lista.add(new Cliente(rset.getInt("IdCliente"), rset.getString("Nombre"), rset.getInt("Telefono")));
+          
+      }
+      finalizarConexion();
+      return lista;
+}
+ 
+    public Cliente obtenerCliente (int id) throws SQLException
+    {
+        Cliente cli = null;
+        
+      ResultSet rset;
+      
+      String sql = "SELECT IdCliente, Nombre, Telefono FROM Cliente WHERE idCliente = ?";
+      PreparedStatement stmt = getConnection().prepareStatement(sql);
+      stmt.setInt(1, id);
+      rset = stmt.executeQuery();
+      while (rset.next())
+      {
+          cli = new Cliente(rset.getInt("IdCliente"), rset.getString("Nombre"), rset.getInt("Telefono"));
+          
+      }
+      finalizarConexion();
+      return cli;
+        
+        
+    }
+    
+    public boolean actualizarCliente(Cliente cli) throws SQLException
+    {
+        boolean result;
+          String sql = "UPDATE cliente SET nombre = ?, telefono = ? WHERE idcliente = ?";
+       PreparedStatement stmt = connection.prepareStatement(sql);
+       stmt.setString(1, cli.getNombre()); //stmt.setString(1, cli.getNombre);
+       stmt.setInt(2, cli.getTelefono());
+       stmt.setInt(3, cli.getIdCliente());
+       
+       int res = stmt.executeUpdate();
+       if (res==0)  
+          result = insertarCliente(cli);
+       else
+           result = true;
+          
+ 
+      return (result);
+    }
+ 
+     
+  public boolean eliminarCliente(int id) throws SQLException
+    {
+        boolean result;
+       String sql = "DELETE FROM cliente WHERE idcliente = ?";
+       PreparedStatement stmt = connection.prepareStatement(sql);
+       stmt.setInt(1, id);
+       
+       int res = stmt.executeUpdate();
+       
+      return (res==1);
+    }
+     
+
+        
+    
+}
---
+++ src/java/rest/GenericResource.java
@@ -0,0 +1,147 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rest;
+
+import bd.Cliente;
+import bd.Conexion;
+import com.google.gson.Gson;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import static javax.ws.rs.HttpMethod.POST;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST Web Service
+ *
+ * @author Lluis_2
+ */
+@Path("generic")
+public class GenericResource {
+
+    @Context
+    private UriInfo context;
+  
+    
+
+    /**
+     * Creates a new instance of GenericResource
+     */
+    public GenericResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of rest.GenericResource
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String listarClientes ()
+    {
+        
+        Conexion conexion = new Conexion();
+        List<Cliente> lista = null;
+        try {
+            lista = conexion.obtenerClientes();
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(GenericResource.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Gson gson = new Gson();
+        
+        return gson.toJson(lista);
+    }
+         
+       
+
+    /**
+     * PUT method for updating or creating an instance of GenericResource
+     * @param content representation for the resource
+     */
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void insertarCliente (String cli) {
+         Conexion conexion = new Conexion();
+         Gson gson = new Gson();
+         Cliente cliente;
+         cliente = gson.fromJson(cli, Cliente.class);
+        try {
+          
+            conexion.insertarCliente(cliente);
+        } catch (SQLException ex) {
+            Logger.getLogger(GenericResource.class.getName()).log(Level.SEVERE, null, ex);
+             
+            
+        }
+        
+    }
+     
+    @GET
+     @Path("{id}")
+     @Produces(MediaType.APPLICATION_JSON)
+     public String mostrarCliente(@PathParam("id") int id)
+     {
+         Cliente cli = null;
+          Conexion conexion = new Conexion();
+         try
+         {
+             cli = conexion.obtenerCliente(id);
+         }
+         catch (SQLException ex) {
+            Logger.getLogger(GenericResource.class.getName()).log(Level.SEVERE, null, ex);
+         }
+         Gson gson = new Gson();
+         
+         return gson.toJson(cli);
+     }
+     
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+     public void actualizarCliente(String cli)
+     {
+          Conexion conexion = new Conexion();
+         Gson gson = new Gson();
+         Cliente cliente;
+         cliente = gson.fromJson(cli, Cliente.class);
+        try {
+          
+            conexion.actualizarCliente(cliente);
+        } catch (SQLException ex) {
+            Logger.getLogger(GenericResource.class.getName()).log(Level.SEVERE, null, ex);
+            
+            
+        }
+     }
+     
+@DELETE
+@Path("/delete/{id}")
+ 
+  
+     public void eliminarCliente( @PathParam("id")int id)
+     {
+          Conexion conexion = new Conexion();
+            
+        try {
+          
+            conexion.eliminarCliente(id);
+        } catch (SQLException ex) {
+            Logger.getLogger(GenericResource.class.getName()).log(Level.SEVERE, null, ex);
+            
+            
+        }
+     }
+}
